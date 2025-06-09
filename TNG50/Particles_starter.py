c0bc3b44867b4b3ffea49ff516decbacbaa8d5e5
Josh Li <j.li20061212@gmail.com>
2025-06-09 14:44:51 -0400

    rotation matrix correctly set up, checkpoint

--- TNG50/Particles_starter.py
+++ TNG50/Particles_starter.py
@@ -378,6 +378,9 @@ if __name__ == '__main__':
     # Primary loop, modified to start from the last known index
     for i in range(1, len(mw_like)):
         print("Iteration:", i)
+        status = input("Continue? (y/n): ")
+        if status == "n":
+            break
         # The current index
         idx = mw_like[i]
 
@@ -385,14 +388,16 @@ if __name__ == '__main__':
         os.makedirs(galaxy_output, exist_ok=True)
 
         theta = 90 # degrees from Z
-        phi = 170 # degrees from X
-        angle = np.deg2rad(180)
+        phi = 0 # degrees from X
+        angle_degrees = 90
+        angle = np.deg2rad(angle_degrees)
+        print(angle)
 
         subfindID0=idx
         a1,a2,a3=create_tags(subfindID0,basePath+'/output/')
         print('Particle numbers halo :',subfindID0,len(np.where(a1)[0]),len(np.where(a2)[0]),len(np.where(a3)[0]))
         angles = []
-        fname = galaxy_output+'/cutout_'+str(subfindID0)+'_' + str(theta) + "_" + str(phi) + '.hdf5'
+        fname = galaxy_output+'/cutout_'+str(subfindID0)+'_' + str(theta) + "_" + str(phi) + "_" + str(angle_degrees) + '.hdf5'
         if os.path.isfile(fname):
             pass
         else:
@@ -596,7 +601,7 @@ if __name__ == '__main__':
         # os.makedirs(os.path.join(bp_local, "cutouts", "results"), exist_ok=True)
         # plt.savefig(bp_local+'/cutouts/results/'+str(idx)+'.png', format='png',dpi=200)
         os.makedirs(os.path.join(galaxy_output, "results"), exist_ok=True)
-        plt.savefig(galaxy_output+'/results/'+str(idx)+'_' + str(theta) + "_" + str(phi) +'.png', format='png',dpi=200)
+        plt.savefig(galaxy_output+'/results/'+str(idx)+'_' + str(theta) + "_" + str(phi) + "_" + str(angle_degrees) +'.png', format='png',dpi=200)
         print("Iteration Complete")
 
 
